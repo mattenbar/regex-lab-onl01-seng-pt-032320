363a8c64440430f49a5c24f54ea68c77103bf704
matthew enbar <matthew.enbar@gmail.com>
2020-04-06 17:30:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ end
 
 def words_five_letters_long(text)
   array = text.scan(/\w{5}/)
-  array.map {|word| word.length == 5}
+  array.collect {|word| word.length == 5}
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
